5796b16cf969f3c57cb444daa1748df9e41e80b8
GonzaCanavesio <gonzalo.canavesio@mi.unc.edu.ar>
2023-06-17 14:27:16 -0300

    Mejora formato unit test

--- Grupal/src/test/java/FeedReaderMainTest.java
+++ Grupal/src/test/java/FeedReaderMainTest.java
@@ -1,25 +1,24 @@
 import org.junit.Test;
 import org.junit.Assert.*;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaPairRDD;
 
 import scala.Tuple2;
 import src.FeedReaderMain;
 
 public class FeedReaderMainTest {
-   @Test 
+    @Test
     public void testParte3() {
-        FeedReaderMain f = new FeedReaderMain();
         SparkConf conf = new SparkConf().setAppName("MiAplicacion").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        
         ArrayList<Tuple2<String, Long>> elementos = new ArrayList<>();
         String searchTerm = "Test";
 
@@ -34,15 +33,15 @@ public class FeedReaderMainTest {
 
         JavaPairRDD<String, Long> wordsRDD = sc.parallelizePairs(elementos);
 
-
-
         JavaPairRDD<Long, Long> orderedIDRDD = FeedReaderMain.doOrder(wordsRDD, searchTerm);
 
         List<Tuple2<Long, Long>> orderedID = orderedIDRDD.collect();
 
-        assert(orderedID.get(0)._1 == 1L);
-        assert(orderedID.get(0)._2 == 3L);
-        assert(orderedID.get(1)._1 == 4L);
-        assert(orderedID.get(1)._2 == 1L);
-    } 
+        sc.close();
+
+        assertTrue (orderedID.get(0)._1 == 1L);
+        assertTrue (orderedID.get(0)._2 == 3L);
+        assertTrue (orderedID.get(1)._1 == 4L);
+        assertTrue (orderedID.get(1)._2 == 1L);
+    }
 }
